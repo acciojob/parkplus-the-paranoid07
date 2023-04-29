0bf0390141b719c3454a882deca8530e831fefb0
Anish <anishpal9621@gmail.com>
2023-04-29 14:09:05 +0530

    fourth try mods

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -66,7 +66,7 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     public Spot updateSpot(int parkingLotId, int spotId, int pricePerHour) {
        // ParkingLot parkingLot=parkingLotRepository1.findById(parkingLotId).get();
 
-        Spot spot=spotRepository1.findById(spotId).get(
+        Spot spot=spotRepository1.findById(spotId).get();
 
         spot.setPricePerHour(pricePerHour);
 
